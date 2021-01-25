4a10ff8c176870d09418aadc4b0790e144b9b4dd
dileigege <1637393255@qq.com>
2021-01-25 18:37:30 +0800

    tablink封装

---
+++ components/homeCommon/tablink/index.vue
@@ -0,0 +1,130 @@
+<template>
+	<view class="CourseEntrance-box">
+		<view class="CourseEntrance-box-Title">
+			<text class="Bo"> 同步教辅 </text> <text class="Lo"> 全科全面的学习工具 </text>
+		</view>
+		<view class="CourseEntrance-box-list clearfix">
+			<view class="discipline " v-for="(item,index) in CourseEntranceData">
+				<view :class="'line'+item.id">
+					<uniIcons class="input-uni-icon" :type="item.type" size="45" color="#fff" />
+					<view class="taba"> {{item.title}} </view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import uniIcons from '@/components/nav_top/uni-icons/uni-icons.vue'
+
+	export default {
+		components: {
+			uniIcons,
+		},
+		props: {
+			CourseEntranceData: Array,
+			type: String,
+			title: String,
+			id: Number
+		},
+		mounted(){
+			console.log(this.CourseEntranceData)
+		}
+	}
+</script>
+
+<style lang="scss">
+	$topColor:#01aefb;
+	$bgcColor:#fff;
+	$fontColor:#999;
+	$fontBColor:#333;
+
+	.CourseEntrance-box {
+		position: absolute;
+		background-color: $bgcColor;
+		width: 80%;
+		top: 10px;
+		left: 50%;
+		transform: translate(-50%);
+		padding: 25rpx 35rpx;
+		border-radius: 15rpx;
+		box-shadow: 1px 2px 9px #e2e2e2;
+
+		.CourseEntrance-box-Title {
+			.Bo {
+				font-weight: 800;
+				font-size: 38rpx;
+				color: $fontBColor;
+				padding-right: 25rpx;
+			}
+
+			.Lo {
+				font-weight: 600;
+				font-size: 25rpx;
+				color: $fontColor;
+			}
+		}
+
+		.CourseEntrance-box-list {
+
+			.discipline {
+				float: left;
+
+
+				// background-color: #E5E5E5;
+
+				.line1 {
+
+					background-color: #bfa0fe;
+					text-align: center;
+					margin: 30rpx 15rpx;
+					padding: 45rpx 29rpx;
+					font-size: 20rpx;
+					font-weight: 800;
+					border-radius: 10rpx;
+					box-shadow: 1px 2px 9px #c7c7c7;
+
+				}
+
+				.line2 {
+					background-color: #4da0ff;
+					text-align: center;
+					margin: 30rpx 10rpx;
+					padding: 45rpx 29rpx;
+					font-size: 20rpx;
+					font-weight: 800;
+					border-radius: 10rpx;
+					box-shadow: 1px 2px 9px #c7c7c7;
+				}
+
+				.line3 {
+					background-color: #fdbb2f;
+					text-align: center;
+					margin: 30rpx 10rpx;
+					padding: 45rpx 29rpx;
+					font-size: 20rpx;
+					font-weight: 800;
+					border-radius: 10rpx;
+					box-shadow: 1px 2px 9px #c7c7c7;
+				}
+
+				.input-uni-icon {
+					font-weight: 900;
+					text-shadow: 1px 2px 9px #5f5f5f;
+				}
+
+				.taba {
+					color: #FFFFFF;
+					font-weight: 900;
+					font-size: 30rpx;
+					padding-top: 15rpx;
+				}
+
+
+			}
+
+
+		}
+
+	}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -3,27 +3,9 @@
 
 		<!-- 课程入口 开始 -->
 		<view class="CourseEntrance">
-			<view class="CourseEntrance-box">
-				<view class="CourseEntrance-box-Title">
-					<text class="Bo"> 同步教辅 </text> <text class="Lo"> 全科全面的学习工具 </text>
-				</view>
-				<view class="CourseEntrance-box-list clearfix">
-					<view class="discipline line1">
-						<uni-icons class="input-uni-icon" type="search" size="45" color="#666666" />
-						<view> 同步教辅 </view>
-					</view>
-
-					<view class="discipline line2">
-						<uni-icons class="input-uni-icon" type="search" size="45" color="#666666" />
-						<view> 同步数学 </view>
-					</view>
-
-					<view class="discipline line3">
-						<uni-icons class="input-uni-icon" type="search" size="45" color="#666666" />
-						<view> 同步英语 </view>
-					</view>
-				</view>
-			</view>
+			<!--  分类 组件  -->
+			<tabLink :CourseEntranceData='CourseEntranceData' :id='id' :type='type' :title='title'> 
+			</tabLink> 
 		</view>
 
 	</view>
@@ -31,14 +13,33 @@
 </template>
 
 <script>
-	import uniIcons from '@/components/nav_top/uni-icons/uni-icons.vue'
+	import tabLink from '@/components/homeCommon/tablink/index.vue'
 	export default {
 		components: {
-			uniIcons,
+			
+			tabLink
 		},
 		data() {
 			return {
-
+				id:0,
+				type:'search',
+				title:'同步教辅',
+				CourseEntranceData: [
+					{
+						id: 1,
+						type: 'search',
+						title: '同步教辅'
+					}, {
+						id: 2,
+						type: 'search',
+						title: '同步数学'
+					},
+					{
+						id: 3,
+						type: 'search',
+						title: '同步英语'
+					},
+				]
 			};
 		}
 	}
@@ -74,55 +75,8 @@
 			background-color: $topColor;
 			height: 150rpx;
 
-			.CourseEntrance-box {
-				position: absolute;
-				background-color: $bgcColor;
-				width: 80%;
-				top: 10px;
-				left: 50%;
-				transform: translate(-50%);
-				padding: 25rpx 35rpx;
-				border-radius: 15rpx;
 
-				.CourseEntrance-box-Title {
-					.Bo {
-						font-weight: 800;
-						font-size: 38rpx;
-						color: $fontBColor;
-						padding-right: 25rpx;
-					}
-
-					.Lo {
-						font-weight: 600;
-						font-size: 25rpx;
-						color: $fontColor;
-					}
-				}
-
-				.CourseEntrance-box-list {
-					
-					.discipline {
-						float: left;
-						text-align: center;
-						width: 25%;
-						// background-color: #E5E5E5;
-						margin: 30rpx 10rpx;
-						padding: 45rpx 15rpx;
-						font-size: 20rpx;
-						font-weight: 800;
-						border-radius: 10rpx;
-						.input-uni-icon{
-							font-weight: 900;
-						}
-						
-					}
-
-					.ine1 {
-						background-color: ;
-					}
-				}
-
-			}
+			
 		}
 	}
 </style>
