1b51c4c18495d80e37331502215cf50370dab04b
dileigege <1637393255@qq.com>
2021-02-05 16:57:55 +0800

    热门列表

--- pages/Question/index.vue
+++ pages/Question/index.vue
@@ -4,7 +4,7 @@
 		<view class="searchTop">
 			<view class="uni-flex uni-row">
 				<view class="title">
-					<text class="titlePiv"> 社区 </text>
+					<text class="titlePiv"> LOGO </text>
 				</view>
 				<view class="button clearfix ">
 					<view class="button1">
@@ -23,20 +23,43 @@
 			<!-- 轮播图 -->
 			<proj-activities :ProjectActivities='ProjectActivities'>
 			</proj-activities>
-
 		</view>
 
 		<!-- 热门问答  -->
-		<view class="hotQuestion-hotTopic">
-			<view class="hotQT-title  uni-flex uni-row full-justified">
-				<text class="text">热门话题</text>
+		<view class="hotQuestion-hotTopic" v-for="(item,index) in hotQuestionHotTopic" :key='index'>
+
+			<view class="hotQT-title  uni-flex uni-row full-justified" v-show="item.hotQTitle.sower == true">
+				<text class="text"> {{item.hotQTitle.Title}} </text>
 				<text class="iconfont fontButtonicon icon-chevron-right">更多</text>
 			</view>
 
+			<view class="hotQT-list-ler">
+				<view class="hotQT-list-title">
+					{{item.hotQTListLer}}
+				</view>
+				<view class="uni-flex uni-row hotQT-list-conter">
+					<image class="flex-imges img" :src="item.url" mode=""></image>
+					<view class="uni-flex uni-column flex-text text">
+						<text>{{item.hotQTListText}}​​​</text>
+					</view>
+				</view>
+
+			</view>
+
+			<view class="hotQT-buttons uni-flex uni-row full-justified pa-tB">
+				<view class="IconText">
+					<text class="iconfont icon-paperclip Icon"></text>
+					<text class="text mr-2"> {{item.reply}} 回复 </text>
+					<text class="text mr-2"> {{item.comments}} 评论 </text>
+				</view>
+				<view class="Icons">
+					<text class=" iconfont Icon icon-heart-empty"></text>
+					<text class=" iconfont Icon icon-publish"></text>
+				</view>
+
+			</view>
+
 		</view>
-
-
-		<!-- 热门话题  -->
 
 		<!-- 底部导航 -->
 		<BottomNavigation>
@@ -84,6 +107,81 @@
 						isboer: false
 					}
 				],
+				hotQuestionHotTopic: [{
+						id: 1,
+						hotQTitle: {
+							sower: true,
+							Title: '热门问答',
+						},
+						hotQTListLer: '有没有可以摘抄下来的神仙句子?',
+						hotQTListText: '很惊艳的那种，想让你快乐 但是不想让别人给你带来快乐，最近经常莫名的笑，原来是又想起你了。 ​​',
+						reply: 125,
+						comments: 900,
+						url: '../../static/home/wallhaven-dpgw2j.jpg'
+					},
+					{
+						id: 2,
+						hotQTitle: {
+							sower: false,
+
+						},
+						hotQTListLer: '有没有可以摘抄下来的神仙句子?',
+						hotQTListText: '很惊艳的那种，想让你快乐 但是不想让别人给你带来快乐，最近经常莫名的笑，原来是又想起你了。 ​​',
+						reply: 125,
+						comments: 900,
+						url: '../../static/home/wallhaven-dpgw2j.jpg'
+					},
+					
+					{
+						id: 3,
+						hotQTitle: {
+							sower: false,
+					
+						},
+						hotQTListLer: '有没有可以摘抄下来的神仙句子?',
+						hotQTListText: '很惊艳的那种，想让你快乐 但是不想让别人给你带来快乐，最近经常莫名的笑，原来是又想起你了。 ​​',
+						reply: 125,
+						comments: 900,
+						url: '../../static/home/wallhaven-dpgw2j.jpg'
+					},
+					
+					{
+						id: 4,
+						hotQTitle: {
+							sower: true,
+							Title: '热门话题',
+						},
+						hotQTListLer: '有没有可以摘抄下来的神仙句子?',
+						hotQTListText: '很惊艳的那种，想让你快乐 但是不想让别人给你带来快乐，最近经常莫名的笑，原来是又想起你了。 ​​',
+						reply: 125,
+						comments: 900,
+						url: '../../static/home/wallhaven-dpgw2j.jpg'
+					},
+					
+					{
+						id: 5,
+						hotQTitle: {
+							sower: false,
+						},
+						hotQTListLer: '有没有可以摘抄下来的神仙句子?',
+						hotQTListText: '很惊艳的那种，想让你快乐 但是不想让别人给你带来快乐，最近经常莫名的笑，原来是又想起你了。 ​​',
+						reply: 125,
+						comments: 900,
+						url: '../../static/home/wallhaven-dpgw2j.jpg'
+					},
+					
+					{
+						id: 6,
+						hotQTitle: {
+							sower: false,
+						},
+						hotQTListLer: '有没有可以摘抄下来的神仙句子?',
+						hotQTListText: '很惊艳的那种，想让你快乐 但是不想让别人给你带来快乐，最近经常莫名的笑，原来是又想起你了。 ​​',
+						reply: 125,
+						comments: 900,
+						url: '../../static/home/wallhaven-dpgw2j.jpg'
+					},
+				]
 			}
 		},
 		methods: {
@@ -141,15 +239,69 @@
 				}
 			}
 		}
-		.hotQuestion-hotTopic{
-			padding:  15rpx 30rpx;
-			border-bottom: 1rpx #e8e8e8 solid;
-			.fontButtonicon{
-				font-size: 30rpx;
+
+		.hotQuestion-hotTopic {
+			padding: 15rpx 30rpx;
+
+			.hotQT-title {
+				padding-bottom: 15rpx;
+				border-bottom: 1rpx #e8e8e8 solid;
+
+				.fontButtonicon {
+					font-size: 30rpx;
+				}
+
+				.text {
+					font-size: 30rpx;
+				}
 			}
-			.text{
-				font-size: 30rpx;
+
+			.hotQT-list-ler {
+				.hotQT-list-conter {
+					.img {
+						width: 150rpx;
+						height: 150rpx;
+						border-radius: 5rpx;
+					}
+
+					.text {
+						margin-left: 25rpx;
+						font-size: 28rpx;
+					}
+
+				}
+
+				.hotQT-list-title {
+					font-size: 38rpx;
+					line-height: 85rpx;
+					font-weight: 300;
+					color: #666666;
+				}
 			}
+
+			.hotQT-buttons {
+				border-bottom: 1rpx solid #e2e2e2;
+
+				.IconText {
+					font-size: 25rpx;
+					color: #666666;
+
+					.text {
+						font-size: 25rpx;
+						color: #d37b62;
+					}
+
+				}
+
+				.Icons {
+					.Icon {
+						font-size: 50rpx;
+						line-height: 58rpx;
+						color: #666666;
+					}
+				}
+			}
+
 		}
 	}
 </style>
